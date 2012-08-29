71a1c2eacdb4b74cb6cf70446fda0e881884775f
Joe Swisher <jaswish@berkeley.edu>
2012-08-29 15:22:45 -0700

    Fixed algorithm to balance at 3 decimal places.

--- jsimutils/balance_charge.py
+++ jsimutils/balance_charge.py
@@ -32,6 +32,7 @@ def main():
     # configure decimal math
     #decimal.getcontext().prec = options.precision
     decimal.getcontext().prec = 8
+    decimal_place = decimal.Decimal('1.000')
     
     # load charge library
     with open(options.library, mode='r') as f:
@@ -65,13 +66,35 @@ def main():
     print '=============================================\ntotal charge: %31.4f' % total_charge
     
     print '\n\ndelta = %f\n\n' % delta
-    print 'New charges'
-    total_charge = decimal.Decimal(0.0)
-    for t,n,q in zip(types, stoichiometry, charges):
-        print '%14s %8.4f x %4d = %12.6f' % (t,q+delta,n,n*(q+delta))
-        total_charge += n*(q+delta)
     
-    print '=============================================\ntotal charge: %31.8f' % total_charge
+    # compute the residual charge when enforcing 3 decimal places
+    residual = decimal.Decimal(0.0)
+    for n,q in zip(stoichiometry, charges):
+        qprime = decimal.Decimal(q+delta).quantize(decimal_place)
+        residual += n*(qprime)
+    
+    if decimal.Decimal('0').compare(residual) is not decimal.Decimal('0'):
+        # check to see if the charge can be evenly divided by any particular pseudo atom type
+        resid_delta = []
+        for t,n in zip(types, stoichiometry):
+            x = residual/n
+            if not x.same_quantum(decimal_place):
+                x = decimal.Decimal(0.000)
+            else:
+                pass
+            resid_delta.append(x)
+    else:
+        resid_delta = [ decimal.Decimal(0.000) for t in types ]
+    
+    # Print final summary
+    total_charge = decimal.Decimal(0.0)
+    print 'New charges'
+    for t,n,q,r in zip(types, stoichiometry, charges, resid_delta):
+        qprime = decimal.Decimal(q + delta - r).quantize(decimal_place)
+        print '%14s %8.4f x %4d = %12.6f' % (t,qprime,n,n*qprime)
+        total_charge += n*qprime    
+    
+    print '=============================================\ntotal charge: %31.6f' % total_charge
     
     return 0
 
