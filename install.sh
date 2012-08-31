53b61b740a1d477d11650e6acaffc169488bb77e
Joe Swisher <jaswish@berkeley.edu>
2012-08-30 17:21:26 -0700

    Updated installation script to detect python version and platform

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-cp -R jsimutils $HOME/Library/Python/2.7/site-packages
+platform=$(uname)
+version=$(python --version 2>&1 | perl -n -e '/Python (\d\.\d)\.\d/ && print $1')
+
+if [[ "$platform" == "Darwin" ]]; then
+  dir=$HOME/Library/Python/"$version"/site-packages
+elif [[ "$platform" == "Linux" ]]; then
+  dir=$HOME/.local/lib/python"$version"/site-packages
+fi
+
+cp -R jsimutils "$dir"
+
+echo "export PYTHONPATH=$dir" >> $HOME/.profile
 
 exit 0
