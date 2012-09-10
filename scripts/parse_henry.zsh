fe42e404e034cca623b90de4a0548439f2c30b9a
Joe Swisher <jaswish@berkeley.edu>
2012-09-10 09:29:13 -0700

    Updated to print numbers uniformly.

--- scripts/parse_henry.zsh
+++ scripts/parse_henry.zsh
@@ -11,8 +11,8 @@ for dir in ${directories[@]}; do
     T=$(awk '/External temperature/ {printf "%.2f", $3}' $f)
     # using perl for awk portability concerns
     mol=$(perl -n -e '/Component 0 +\[(\w+)\] +\(Adsorbate/ && print $1' < $f)
-    W=($(awk '/\['"$mol"'\] Average Widom/ {print $4,$6}' $f))
-    KH=($(awk '/\['"$mol"'\] Average Henry/ {print $5,$7}' $f))
+    W=($(awk '/\['"$mol"'\] Average Widom/ {printf "%.4e %.4e",$4,$6}' $f))
+    KH=($(awk '/\['"$mol"'\] Average Henry/ {printf "%.4e %.4e",$5,$7}' $f))
     RT=$((0.0083145*$T))
     dH=($(awk '/\['"$mol"'\] Average +<U_/ {printf "%.3f %.2e\n",($9-'"$RT"'),$11}' $f))
     echo "$fwk $T $mol ${W[@]} ${KH[@]} ${dH[@]}"
