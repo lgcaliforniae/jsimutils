c8ae867d17a25da4897ddc52d46b2a26bb9bf9ed
Joe Swisher <jaswish@berkeley.edu>
2012-09-24 10:58:36 -0700

    Detects and works on compressed scripts.

--- scripts/parse_henry.zsh
+++ scripts/parse_henry.zsh
@@ -6,7 +6,11 @@ directories=($@)
 echo "- K - - - mol/(kg*Pa) mol/(kg*Pa) kJ/mol kJ/mol"
 
 for dir in ${directories[@]}; do
-  for f in `find "$dir" -name "*.data"`; do
+  for f in `find "$dir" -name "*.data" -or -name "output.0.log.gz"`; do
+    if [[ ${f:e} == "gz" ]]; then
+      gunzip -c $f > tmp
+      f=tmp
+    fi
     fwk=$(awk '/Framework name:/ {print $3}' $f)
     T=$(awk '/External temperature/ {printf "%.2f", $3}' $f)
     # using perl for awk portability concerns
